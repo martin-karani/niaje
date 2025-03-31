62abcea13935962ad56143fb109abd066213271f
Martin Karani <karanimartinmk@gmail.com>
2025-03-31 23:54:08 +0300

    add authentication provider and sync component; update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-import Provider from "@/app/provider";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-import { ClerkProvider } from "@clerk/nextjs";
-import { Analytics } from "@vercel/analytics/react";
 import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
 import "./globals.css";
+import { AuthTokenSync } from "@/lib/auth-sync";
+import { AuthProvider } from "@/providers/auth-provider";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://nextstarter.xyz/"),
@@ -43,23 +42,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider dynamic>
-      <html lang="en" suppressHydrationWarning>
-        <body className={GeistSans.className}>
-          <Provider>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="dark"
-              enableSystem
-              disableTransitionOnChange
-            >
-              {children}
-              <Toaster />
-            </ThemeProvider>
-          </Provider>
-          <Analytics />
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={GeistSans.className}>
+        <AuthProvider>
+          {/* Component to sync localStorage auth tokens with cookies for SSR */}
+          <AuthTokenSync />
+          
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </AuthProvider>
+      </body>
+    </html>
   );
 }
